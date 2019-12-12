7c14c0d00c4cd9bf9a798b675b5232b21f7ca99d
tobiasfurtlehner <fur17466@spengergasse.at>
2019-12-12 16:26:00 +0100

    Initial Commit

---
+++ advent_of_code_pos/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  advent_of_code_pos
+//
+//  Created by Tobias Furtlehner on 12.12.19.
+//  Copyright © 2019 Tobias Furtlehner. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
